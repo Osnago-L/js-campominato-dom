3d7a1f9ec52069e78ba7c79025a0a7e2bab34f35
Osnago-L <osna.brera@gmail.com>
2021-12-21 16:57:03 +0100

    error

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,20 +1,21 @@
 function difficultyStart(){
     let bombs = [];
+    let score = 0;
     let difficulty = document.getElementById("difficulty").value;
     document.querySelector(".maingrid").innerHTML = "";
     if(difficulty == "easy"){
         for (let i = 1; i <= 100; i++) {
-            let square = `<div id="item-${i}" class="grid10 item"><h1 id="numitem-${i}" class="numbox">${i}</h1></div>`;
+            let square = `<div id="item-${i}" class="grid10 item">${i}</div>`;
             document.querySelector(".maingrid").innerHTML += square;
         }
     }else if(difficulty == "medium"){
         for (let i = 1; i <= 81; i++) {
-            let square = `<div id="item-${i+1}" class="grid9 item"><h1 id="numitem-${i+1}" class="numbox">${i}</h1></div>`;
+            let square = `<div id="item-${i}" class="grid9 item">${i}</div>`;
             document.querySelector(".maingrid").innerHTML += square;
         }
     }else{
         for (let i = 1; i <= 49; i++) {
-            let square = `<div id="item-${i+1}" class="grid7 item"><h1 id="numitem-${i+1}" class="numbox">${i}</h1></div>`;
+            let square = `<div id="item-${i}" class="grid7 item">${i}</div>`;
             document.querySelector(".maingrid").innerHTML += square;
         }
     }
@@ -32,16 +33,44 @@ function difficultyStart(){
                         i--;
                     }
             }
-        }     
+            console.log(bombs)
+        }   
+
         for(let i = 0; i < listItem.length; i++) {
-            listItem[i].addEventListener("click", function() {
-                let numtest= parseInt(document.getElementById('numitem-'+(i+1)).textContent);
-                if (bombs.includes(numtest)==true) {
-                    this.style.backgroundColor = "#DC143C";
-                }else{
-                    this.style.backgroundColor = "#6495ED";
-                }
-            });
+            listItem[i].addEventListener("click", checkBombs(listItem[i]));
         }
+
+        function checkBombs(test){
+            console.log(test)
+            let numtest= parseInt(test.innerHTML);
+            if (bombs.includes(numtest)==true) {
+                test.style.backgroundColor = "#DC143C";
+                // gameLost();
+            }else{
+                test.style.backgroundColor = "#6495ED";
+                score += 100;
+            }
+        }
+
+        // function gameLost(){
+        //     for(let i = 0; i < listItem.length; i++) {
+        //         listItem[i].removeEventListener("click", checkBombs());
+        //             let numtest= parseInt(this.innerHTML);
+        //             if (bombs.includes(numtest)==true) {
+        //                 this.style.backgroundColor = "#DC143C";
+        //             }
+                
+        //     }
+        // }
+
+        
 }
 
+
+
+// for( let t = 0; t < 10; t++ )
+//             {
+//                 setTimeout( function(){
+//                     console.log("wait");
+//                 }, 1000 * a )
+//             }
