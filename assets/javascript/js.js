581c1bcbdc2c97345f2f2f5f3b9382684b5d9c28
Osnago-L <osna.brera@gmail.com>
2021-12-21 15:40:57 +0100

    red bombs added

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,30 +1,28 @@
 function difficultyStart(){
+    let bombs = [];
     let difficulty = document.getElementById("difficulty").value;
     document.querySelector(".maingrid").innerHTML = "";
     if(difficulty == "easy"){
         for (let i = 1; i <= 100; i++) {
-            let square = `<div id="item-${i}" class="grid10 item"><h1 class="numbox">${i}</h1></div>`;
+            let square = `<div id="item-${i}" class="grid10 item"><h1 id="numitem-${i}" class="numbox">${i}</h1></div>`;
             document.querySelector(".maingrid").innerHTML += square;
         }
     }else if(difficulty == "medium"){
         for (let i = 1; i <= 81; i++) {
-            let square = `<div id="item-${i}" class="grid9 item"><h1 class="numbox">${i}</h1></div>`;
+            let square = `<div id="item-${i+1}" class="grid9 item"><h1 id="numitem-${i+1}" class="numbox">${i}</h1></div>`;
             document.querySelector(".maingrid").innerHTML += square;
         }
     }else{
         for (let i = 1; i <= 49; i++) {
-            let square = `<div id="item-${i}" class="grid7 item"><h1 class="numbox">${i}</h1></div>`;
+            let square = `<div id="item-${i+1}" class="grid7 item"><h1 id="numitem-${i+1}" class="numbox">${i}</h1></div>`;
             document.querySelector(".maingrid").innerHTML += square;
         }
     }
         const listItem = document.querySelectorAll(".item");
-
-        let bombs = [];
-
-        functionBombs(listItem);
         
-        function functionBombs() {
-            
+        functionBombs();
+        
+        function functionBombs() {  
             for (let i = 0; i < 16; i++) {
                 let randomBombs = Math.floor(Math.random()*listItem.length +1);
                 let truetest = bombs.includes(randomBombs);
@@ -34,14 +32,11 @@ function difficultyStart(){
                         i--;
                     }
             }
-            console.log(bombs);
-            return bombs;
-        }
-        
-        
+        }     
         for(let i = 0; i < listItem.length; i++) {
             listItem[i].addEventListener("click", function() {
-                if (bombs.includes(listItem[i])==true) {
+                let numtest= parseInt(document.getElementById('numitem-'+(i+1)).textContent);
+                if (bombs.includes(numtest)==true) {
                     this.style.backgroundColor = "#DC143C";
                 }else{
                     this.style.backgroundColor = "#6495ED";
